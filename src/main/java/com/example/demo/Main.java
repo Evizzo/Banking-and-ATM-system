f6a5dc5cb7b519ab4cae4b04ac35bc76957fd9ae
Evizzo <stefanmatic941@gmail.com>
2022-11-13 10:50:40 +0100

    Removing javaFX

--- src/main/java/com/example/demo/Main.java
+++ src/main/java/com/example/demo/Main.java
@@ -1,31 +1,7 @@
 package com.example.demo;
 
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.scene.image.Image;
-import javafx.stage.Stage;
-
-import java.io.IOException;
-import java.util.Objects;
-
-public class Main extends Application {
+public class Main {
     public static void main(String[] args) {
-        launch(args);
-    }
-    @Override
-    public void start(Stage stage) {
-        Parent root;
-        try {
-            root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("MainScreen.fxml")));
-        } catch (IOException e) {
-            throw new AppException("loadMainScreen function failed", e);
-        }
-        stage.setTitle("Banking and ATM system");
-        Image icon = new Image("logoatm.png");
-        stage.getIcons().add(icon);
-        stage.setScene(new Scene(root));
-        stage.show();
+
     }
 }
